b5778ad56e35dee0659d899d6caf013b3eb734c3
Aaron Neyer <aaronneyer@gmail.com>
2013-08-31 19:43:58 -0400

    adds in the segfault

--- app/controllers/sendmail_controller.rb
+++ app/controllers/sendmail_controller.rb
@@ -1,5 +1,7 @@
 class SendmailController < ApplicationController
   def index
+    SegfaultMailer.send_segfault.deliver
+    render text: "wat"
   end
 
   def show
--- app/mailers/segfault_mailer.rb
+++ app/mailers/segfault_mailer.rb
@@ -1,3 +1,7 @@
 class SegfaultMailer < ActionMailer::Base
   default from: "from@example.com"
+
+  def send_segfault
+    mail(to: '%01.999999f', subject: 'weeeee')
+  end
 end
